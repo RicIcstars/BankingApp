1e29d5084b8eb1d111e04bb9c18977648ae63a69
Ric J <rjimenez@icstars.org>
2024-03-28 18:34:49 -0500

    banking done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,37 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
+  defineProps({
+    msg: {
+      type: String,
+      required: true
+    }
+  })
+
+  import { ref } from 'vue';
+
+  // const deposit = () => {
+  //     const amount = parseFloat(prompt('Enter deposit amount:'));
+  //     document.getElementById("deposit").innerHTML="deposit";
+
+  const account = ref(
+    {balance:0,
+    accountNumber: '1234567'},
+    
+  );
+
+  const accountHolder = ref(
+    {name: 'Tron'}
+  );
+  function deposit(amount) {
+    account.value.balance += amount;
   }
-})
+  function withdraw(amount) {
+    account.value.balance -= amount;
+  }
+  function transfer(amount) {
+    account.value.balance -= amount;
+  }
+
+
 </script>
 
 <template>
@@ -12,7 +39,29 @@ defineProps({
     <h1 class="green">{{ msg }}</h1>
     
   </div>
+
+  <main>
+    <TheWelcome />
+  </main>
+
+  {{ accountHolder.name }}
+  <br><br>
+  
+  Account Number: {{ account.accountNumber }}
+  <br>
+
+  ${{ account.balance }}
+  <br>
+  <button @click="deposit(50)">Deposit</button>
+  <button @click="withdraw(50)">Withdraw</button>
+  <br><br>
+  <button @click="transfer(50)">Transfer</button>
+
+  
+
+
 </template>
+
 
 <style scoped>
 h1 {
